fd47e410b04f7d504bab902ae40861a10b543816
박준우 <wnsdn2186@naver.com>
2023-04-11 22:41:03 +0900

    #2 Make Main UI

--- app/src/main/java/com/example/zzingzzing/MainActivity.kt
+++ app/src/main/java/com/example/zzingzzing/MainActivity.kt
@@ -10,5 +10,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction().replace(R.id.mainFrame, CollectFragment()).commit()
+
+        binding.btnMoveHome.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.mainFrame, CollectFragment()).commit()
+        }
+
+        binding.btnMoveMessageList.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.mainFrame, MessageListFragment()).commit()
+        }
+
+        binding.btnMoveTelList.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.mainFrame, TelListFragment()).commit()
+        }
     }
 }
