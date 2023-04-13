c4c284a590c1b73389b7b3ef6ebe644168cc0c82
박준우 <wnsdn2186@naver.com>
2023-04-13 23:09:08 +0900

    #3 Make FullScreen and Add Arrow Button

--- app/src/main/java/com/example/zzingzzing/SplashActivity.kt
+++ app/src/main/java/com/example/zzingzzing/SplashActivity.kt
@@ -1,11 +1,16 @@
 package com.example.zzingzzing
 
 import android.content.Intent
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.PersistableBundle
+import android.os.*
+import android.os.Build.VERSION
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.example.zzingzzing.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -16,12 +21,11 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 일정 시간 지연 이후 실행하기 위한 코드
         Handler(Looper.getMainLooper()).postDelayed({
             // 일정 시간이 지나면 MainActivity로 이동
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000) // 시간 0.5초 이후 실행
+        }, 3000)
     }
 }
